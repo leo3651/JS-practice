dddb7ac4d40443172dbfac217bb766b07a0280a8
Leo <leo.kowacevic@gmail.com>
2024-02-03 20:31:37 +0100

    add again and highscore

--- 03 guess my number/script.js
+++ 03 guess my number/script.js
@@ -13,8 +13,9 @@ document.querySelector(".guess").value = 19;
 console.log(document.querySelector(".guess").value); */
 
 let score = 20;
-const secretNumber = Math.floor(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let highscore = 0;
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+// document.querySelector(".number").textContent = secretNumber;
 console.log(secretNumber);
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -28,6 +29,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector("highscore").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high";
@@ -50,4 +56,15 @@ document.querySelector(".check").addEventListener("click", function () {
 
   // console.log(document.querySelector(".guess").value);
   // document.querySelector(".message").textContent = "Correct number";
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
 });
