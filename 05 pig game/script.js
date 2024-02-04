c3110a14adb6dc15172650e018d8d1e0d1639ebc
Leo <leo.kowacevic@gmail.com>
2024-02-04 19:05:14 +0100

    add hold btn

--- 05 pig game/script.js
+++ 05 pig game/script.js
@@ -4,35 +4,53 @@
 const score0Element = document.getElementById("score--0");
 const score1Element = document.getElementById("score--1");
 const diceElement = document.querySelector(".dice");
-const rollDiceElement = document.querySelector(".btn--roll");
+const rollBtn = document.querySelector(".btn--roll");
+const newGameBtn = document.querySelector(".btn--new");
+const holdBtn = document.querySelector(".btn--hold");
 const player0Element = document.querySelector(".player--0");
 const player1Element = document.querySelector(".player--1");
 const currentScorePlayer0 = document.getElementById("current--0");
 const currentScorePlayer1 = document.getElementById("current--1");
 
+// initial state
 score0Element.textContent = 0;
 score1Element.textContent = 0;
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 diceElement.classList.add("hidden");
 
-rollDiceElement.addEventListener("click", function () {
+// roll the dice
+rollBtn.addEventListener("click", function () {
+  // generate number display the dice
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   diceElement.classList.remove("hidden");
   diceElement.setAttribute("src", `dice-${randomNumber}.png`);
+  // check if 1
   if (randomNumber === 1) {
-    if (player0Element.classList.contains("player--active")) {
-      player0Element.classList.remove("player--active");
-      player1Element.classList.add("player--active");
-    } else {
-      player1Element.classList.remove("player--active");
-      player0Element.classList.add("player--active");
-    }
-  } else {
-    if (player0Element.classList.contains("player--active")) {
-      currentScorePlayer0.textContent =
-        Number(currentScorePlayer0.textContent) + randomNumber;
-    } else {
-      currentScorePlayer1.textContent =
-        Number(currentScorePlayer1.textContent) + randomNumber;
-    }
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle("player--active");
+    player1Element.classList.toggle("player--active");
   }
+  // check if 2-6
+  else {
+    currentScore += randomNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  }
+});
+
+holdBtn.addEventListener("click", function () {
+  scores[activePlayer] += currentScore;
+  score0Element.textContent = scores[0];
+  score1Element.textContent = scores[1];
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle("player--active");
+  player1Element.classList.toggle("player--active");
 });
