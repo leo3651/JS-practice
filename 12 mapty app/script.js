e3018f49056c69db5bbae454a604bc2bbbb826f5
Leo <leo.kowacevic@gmail.com>
2024-02-19 21:57:51 +0100

    add map, place marker

--- 12 mapty app/script.js
+++ 12 mapty app/script.js
@@ -1,12 +1,81 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},13.8z?entry=ttu`
+      );
+
+      const coords = [latitude, longitude];
+      map = L.map("map").setView(coords, 13);
+      console.log(map);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+
+      map.on("click", function (mapE) {
+        form.classList.remove("hidden");
+        inputDistance.focus();
+        mapEvent = mapE;
+      });
+    },
+    function () {
+      alert("Couldn't get the position");
+    }
+  );
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("sumbited");
+
+  inputDistance.value = inputDuration.value = "";
+
+  console.log(mapEvent);
+  const { lat: latitude, lng: longitude } = mapEvent.latlng;
+  console.log(latitude, longitude);
+  const coords = [latitude, longitude];
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
