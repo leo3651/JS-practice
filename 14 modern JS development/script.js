4de5afecfdcd87d3f2b0bef678d9af13f0854623
Leo <leo.kowacevic@gmail.com>
2024-02-25 15:27:06 +0100

    imports exports

--- 14 modern JS development/script.js
+++ 14 modern JS development/script.js
@@ -1,4 +1,36 @@
+///////////////////////////////////
+/////// EXPORTING AND IMPORTING IN ES6 MODULES
+///////////////////////////////////
 // importing module
+import { addToCart, cart, totalPrice as price, tq } from "./shoppingCart.js";
 
-import "./shoppingCart.js";
 console.log("importing module");
+// console.log(shippingCost); //must be exported from shoppingCart
+
+addToCart("bread", 5);
+console.log(cart);
+
+console.log(price, tq);
+
+///
+import * as ShoppingCart from "./shoppingCart.js";
+
+console.log(ShoppingCart);
+
+ShoppingCart.addToCart("potato", 1);
+console.log(ShoppingCart.totalPrice);
+console.log(ShoppingCart.tq);
+
+// import default
+import add from "./shoppingCart.js";
+// import add2, {
+//   addToCart as addToCart2,
+//   cart as cart2,
+//   totalPrice as price2,
+//   tq as tq2,
+// } from "./shoppingCart.js"; // can be mixed, NOT adviceable
+
+console.log(add);
+// console.log(add2);
+add("watermelon", 3);
+console.log(cart);
--- 14 modern JS development/shoppingCart.js
+++ 14 modern JS development/shoppingCart.js
@@ -1,2 +1,24 @@
+///////////////////////////////////
+/////// EXPORTING AND IMPORTING IN ES6 MODULES
+///////////////////////////////////
 // exporting module
 console.log("exporting module");
+
+// scoped to the current module
+const nameOfStore = "Shoppers";
+const shippingCost = 10;
+export const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to the cart`);
+};
+
+let totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to the cart`);
+}
