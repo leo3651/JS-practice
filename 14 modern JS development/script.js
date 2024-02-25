bf7907eae682a3d5b7f6ef9b0e8835ddfda6386c
Leo <leo.kowacevic@gmail.com>
2024-02-25 17:59:11 +0100

    the module pattern

--- 14 modern JS development/script.js
+++ 14 modern JS development/script.js
@@ -34,3 +34,67 @@ console.log(add);
 // console.log(add2);
 add("watermelon", 3);
 console.log(cart);
+
+///////////////////////////////////
+/////// TOP LEVEL AWAIT
+///////////////////////////////////
+console.log("Start fetching");
+const results = await fetch("https://jsonplaceholder.typicode.com/posts"); // blocks execution of entire module
+const posts = await results.json();
+console.log(posts);
+
+console.log("After top level await");
+
+///
+const getLastPost = async function () {
+  const results = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await results.json();
+  // console.log(posts);
+
+  return { title: posts.at(-1).title, text: posts.at(-1).body };
+};
+
+getLastPost();
+const lastPost = getLastPost();
+console.log(lastPost);
+
+getLastPost().then((post) => console.log(post)); // not very clean
+
+console.log("Something");
+
+const resolvedPromiseLastPost = await getLastPost();
+console.log(resolvedPromiseLastPost);
+
+console.log("End");
+
+///////////////////////////////////
+/////// THE MODULE PATTERN
+///////////////////////////////////
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shoppingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to the cart`);
+  };
+
+  const order = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered`);
+  };
+
+  return { cart, totalPrice, totalQuantity, addToCart };
+})();
+
+console.log(shoppingCart2);
+
+console.log(shoppingCart2.totalPrice);
+shoppingCart2.totalPrice = 10;
+console.log(shoppingCart2.totalPrice);
+
+shoppingCart2.addToCart("bananas", 10);
+console.log(shoppingCart2);
+
+console.log(shoppingCart2.shoppingCost);
--- 14 modern JS development/shoppingCart.js
+++ 14 modern JS development/shoppingCart.js
@@ -22,3 +22,10 @@ export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to the cart`);
 }
+
+///////////////////////////////////
+/////// TOP LEVEL AWAIT
+///////////////////////////////////
+console.log("Start fetching users");
+await fetch("https://jsonplaceholder.typicode.com/posts");
+console.log("Finish fetching users");
