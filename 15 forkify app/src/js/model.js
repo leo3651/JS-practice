8eb3eff13549cce9ab1273d7b35393822bbbb9d5
Leo <leo.kowacevic@gmail.com>
2024-02-27 18:51:18 +0100

    build architecture

--- 15 forkify app/src/js/model.js
+++ 15 forkify app/src/js/model.js
@@ -5,28 +5,33 @@ export const state = {
 };
 
 export const loadRecipe = async function (id) {
-  const response = await fetch(
-    // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+  try {
+    const response = await fetch(
+      // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
 
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
-  const results = await response.json();
-  console.log(response);
-  console.log(results);
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const results = await response.json();
+    console.log(response);
+    console.log(results);
 
-  if (!response.ok) throw new Error(`${results.message} (${response.status})`);
+    if (!response.ok)
+      throw new Error(`${results.message} (${response.status})`);
 
-  const { recipe } = results.data;
-  console.log(recipe);
-  state.recipe = {
-    id: recipe.id,
-    cookingTime: recipe.cooking_time,
-    imageUrl: recipe.image_url,
-    ingredients: recipe.ingredients,
-    publisher: recipe.publisher,
-    servings: recipe.servings,
-    sourceUrl: recipe.source_url,
-    title: recipe.title,
-  };
-  console.log(state.recipe);
+    const { recipe } = results.data;
+    console.log(recipe);
+    state.recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
 };
