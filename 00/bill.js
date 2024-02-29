617c323377aefadc3b73e8795eb25c957ae9faec
Leo <leo.kowacevic@gmail.com>
2024-02-29 16:42:57 +0100

    render bills

--- 00/bill.js
+++ 00/bill.js
@@ -1,17 +1,30 @@
 class Bill {
-  #totalPrice = this.quantity * this.price;
-  constructor(billNumber, serviceName, startDate, endDate, people, price) {
+  #totalPrice;
+  constructor(
+    billNumber = 123,
+    serviceName = 123,
+    startDate = "08.06.2022",
+    endDate = "11.06.2022",
+    people = 1,
+    price = 100,
+    creditCard = "Visa",
+    latest1 = "08.06.2022, 11:00",
+    latest2 = "08.06.2022, 15:00"
+  ) {
     this.billNumber = billNumber;
     this.serviceName = serviceName;
     this.startDate = startDate;
     this.endDate = endDate;
     this.people = people;
     this.price = price;
+    this.creditCard = creditCard;
+    (this.latest1 = latest1), (this.latest2 = latest2);
 
     this.calcQuantity();
+    this.#generateFirstTable();
   }
 
-  #generateMarkup() {
+  #generateFirstTable() {
     const html = `
     <h1>Bill num. ${this.billNumber} for accomodation service</h1>
       <table>
@@ -28,35 +41,75 @@ class Bill {
           <tr>
             <td>${this.serviceName}</td>
             <td class="light-borders">${this.startDate} - ${this.endDate}</td>
-            <td>${this.people} ${this.people > 1 ? "osobe" : "osoba"}</td>
+            <td>${this.people} ${this.people > 1 ? "people" : "preson"}</td>
             <td>${this.price} EUR</td>
-            <td>${this.quantity} ${
-      this.quantity > 1 ? "noćenja" : "noćenje"
-    }</td>
-            <td style="font-weight: normal">700 EUR</td>
+            <td>${this.quantity} ${this.quantity > 1 ? "nights" : "night"}</td>
+            <td style="font-weight: normal">${this.#totalPrice} EUR</td>
           </tr>
           <tr>
             <td colspan="6" style="color: transparent">Empty</td>
           </tr>
           <tr style="background-color: lightblue">
-            <td colspan="5">Ukupno</td>
+            <td colspan="5">Total</td>
 
             <td>${this.#totalPrice} EUR</td>
           </tr>
         </tbody>
       </table>
     `;
+
+    this.renderHtml(html);
+  }
+
+  #generateSecondTable() {
+    const html = `
+    <table>
+        <thead>
+          <tr>
+            <th>Payment</th>
+            <th>Payment method</th>
+            <th>Time of payment</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Advence payment</td>
+            <td>Credit card (${this.creditCard})</td>
+            <td>Latest till ${this.latest1}</td>
+            <td>${this.#totalPrice / 2} EUR</td>
+          </tr>
+          <tr>
+            <td>Advence payment</td>
+            <td>Credit card (${this.creditCard})</td>
+            <td>Latest till ${this.latest2}</td>
+            <td>${this.#totalPrice / 2} EUR</td>
+          </tr>
+        </tbody>
+      </table>`;
   }
 
   calcQuantity() {
     const differenceInMs =
-      new Date(this.endDate).getTime - new Date(this.startDate).getTime();
+      new Date(this.formatDate(this.endDate)).getTime() -
+      new Date(this.formatDate(this.startDate)).getTime();
     this.quantity = differenceInMs / (1000 * 3600 * 24);
+    this.#totalPrice = this.quantity * this.price;
   }
 
-  renderHtml() {
-    document.querySelector("main").insertAdjacentHTML("afterbegin", html);
+  renderHtml(html) {
+    document.querySelector(".root").insertAdjacentHTML("afterbegin", html);
+  }
+
+  formatDate(date) {
+    return new Intl.DateTimeFormat("HR-hr", {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    }).format(new Date(date));
   }
 }
 
-const bill = new Bill(12345, "AS-12345", "08.06.2022", "09.06.2022");
+// const bill = new Bill(12345, "AS-12345", "06.08.2022", "06.09.2022", 5, 103);
+
+const bill2 = new Bill(12345, "AS-12345", "08.06.2022", "10.6.2022", 5, 103);
