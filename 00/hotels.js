3a602846168fdc91852494f6547a06e66562c365
Leo <leo.kowacevic@gmail.com>
2024-03-01 18:49:48 +0100

    render hotels

--- 00/hotels.js
+++ 00/hotels.js
@@ -6,9 +6,40 @@ const getData = async function () {
     console.log(results);
 
     if (!response.ok) throw new Error("Couldn't get data");
+    return results;
   } catch (err) {
     alert(err);
   }
 };
 
-getData();
+const renderData = async function () {
+  const data = await getData();
+  console.log(data);
+  let html;
+  data.forEach((element) => {
+    html += `
+    <div class="card grid">
+      <figure class="img-box">
+        <img src="${element.image}" alt="Photo" />
+      </figure>
+      <div class="content">
+        <h2>${element.title}</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+          cupiditate eaque sequi doloremque nemo esse, facere eveniet quae
+          aspernatur cum, enim odio consectetur fugiat sed obcaecati. Odio
+          recusandae sed voluptates.
+        </p>
+        <a class="check-hotel-btn" href="#">Check hotel</a>
+      </div>
+    </div>
+    `;
+  });
+
+  document.querySelector(".card-container").innerHTML = "";
+  document
+    .querySelector(".card-container")
+    .insertAdjacentHTML("afterbegin", html);
+};
+
+renderData();
