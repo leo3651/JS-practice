b7ee4dacb9dc758ecb46b0337911328e32a2534f
Leo <leo.kowacevic@gmail.com>
2024-03-01 22:03:16 +0100

    filtering

--- 00/hotels.js
+++ 00/hotels.js
@@ -1,3 +1,26 @@
+const inputStartDate = document.querySelector(".input-start-date");
+const inputEndDate = document.querySelector(".input-end-date");
+const capacity = document.getElementById("capacity");
+const airConditioning = document.getElementById("air-conditioning");
+const parkingSpace = document.getElementById("parking-space");
+const pets = document.getElementById("pets");
+const pool = document.getElementById("pool");
+const wifi = document.getElementById("wifi");
+const tv = document.getElementById("TV");
+const filterBtn = document.querySelector(".filter-btn");
+console.log(
+  inputStartDate,
+  inputEndDate,
+  capacity,
+  airConditioning,
+  parkingSpace,
+  pets,
+  pool,
+  wifi,
+  tv,
+  filterBtn
+);
+
 const getData = async function () {
   try {
     const response = await fetch("https://api.adriatic.hr/test/accommodation");
@@ -12,9 +35,42 @@ const getData = async function () {
   }
 };
 
-const renderData = async function () {
-  const data = await getData();
+const renderData = async function (
+  startDate = "",
+  endDate = "",
+  capacity = "",
+  airConditioning = false,
+  parkingSpace = false,
+  pets = false,
+  pool = false,
+  wifi = false,
+  tv = false
+) {
+  let data = await getData();
   console.log(data);
+  if (startDate || endDate) {
+    if (!new Date(startDate).getTime() || !new Date(endDate).getTime()) {
+      console.log(startDate, endDate);
+      alert("Invalid dates");
+      console.log(new Date(startDate).getTime());
+      return;
+    }
+    data = data.filter((hotel) => {
+      for (let i = 0, n = hotel.availableDates.length; i < n; i++) {
+        if (
+          new Date(hotel.availableDates[i].intervalStart).getTime() <
+            new Date(startDate).getTime() &&
+          new Date(hotel.availableDates[i].intervalEnd).getTime() >
+            new Date(endDate).getTime()
+        ) {
+          console.log("in");
+          return hotel;
+        }
+      }
+    });
+  }
+  console.log("Leo");
+
   let html = "";
   data.forEach((element) => {
     html += `
@@ -51,3 +107,17 @@ const renderData = async function () {
 };
 
 renderData();
+
+filterBtn.addEventListener("click", function () {
+  renderData(
+    inputStartDate.value,
+    inputEndDate.value,
+    capacity.value,
+    airConditioning.checked,
+    parkingSpace.checked,
+    pets.checked,
+    pool.checked,
+    wifi.checked,
+    tv.checked
+  );
+});
